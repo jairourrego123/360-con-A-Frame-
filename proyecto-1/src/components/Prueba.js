c2c86bc4105caf56978f98b55603b0f067ca8cf4
jairourrego123 <jairourrego123@gmail.com>
2022-09-26 08:22:58 -0500

    botones de slaida y entrada

--- proyecto-1/src/components/Prueba.js
+++ proyecto-1/src/components/Prueba.js
@@ -17,10 +17,18 @@ function Prueba() {
         <img id="ingenieria" src="img/ingenieria.jpg" alt="ingenieria" />
         <img id="campus" src="img/campus.jpg" alt="campus" />
         <img id="conciliacion" src="img/conciliacion.jpg" alt="conciliacion" />
+        <img id="entrada" src="img/entrada.png" alt="entrada" />
+        <img id="entradaposgrados" src="img/campus.jpg" alt="entradaposgrados" />
+        <img id="salidaposgrados" src="img/ugc.jpg" alt="salidaposgrados" />
+        <img id="salida" src="img/salida.png" alt="salida" />
         <audio id="alerta" src="sound/alerta-nextel-ringtones.mp3" preload="auto"></audio>
         <audio id="gallo" src="sound/Gallo.mp3" preload="auto"></audio>
         <audio id="tuberia" src="sound/mario-bros tuberia.mp3" preload="auto"></audio>
         <audio id="mario" src="sound/mario-bros.mp3" preload="auto"></audio>
+        <audio id="sound-entrada" src="sound/mario-bros.mp3" preload="auto"></audio>
+        <audio id="sound-salida" src="sound/mario-bros tuberia.mp3" preload="auto"></audio>
+        <audio id="click" src="sound/alerta-nextel-ringtones.mp3" preload="auto"></audio>
+
         {/* <Imagen/> */}
       </a-assets>
 
@@ -28,62 +36,84 @@ function Prueba() {
         <a-cursor raycaster="objects: .clickable" ></a-cursor>
       </a-camera>
 
-      <a-entuty>
+      <a-entity position="-1.9 1.3 -5">
 
 
-      {/* -------------------------------------Ingenieria--------------------------------- */}
+        {/* -------------------------------------Ingenieria--------------------------------- */}
+        <a-entity>
+          <a-box scale="0.25 0.25 0.25" rotation="45 45 45" color="red" visible="false"
+            animation__position="property:rotation;to:360 0 0; dir:alternate;dur:3000;loop:true"></a-box>
 
-      <a-box position="-1.9 1.3 -3" scale="0.25 0.25 0.25" rotation="45 45 45" color="red"
-        animation__position="property:rotation;to:360 0 0; dir:alternate;dur:3000;loop:true"></a-box>
+          <a-plane color="black" position="1.9 0 0" width="3" height="0.25" class="clickable"
+            change-site="img: #ingenieria "
+            sound="on:mouseenter; src:#gallo">
 
-      <a-plane color="black" position="0 1.3 -3" width="3" height="0.25" class="clickable"
-        change-site="img: #ingenieria"
-        sound="on:mouseenter; src:#gallo">
-
-        <a-text value="Ingenieria" color="white" align="center"></a-text>
-      </a-plane>
-
-
-      {/* ----------------------------------Posgrados---------------------------------- */}
-
-      <a-box position="-1.9 1 -3" scale="0.25 0.25 0.25" rotation="45 45 45" color="red"
-        animation__position="property:rotation;to:360 0 0; dir:alternate;dur:3000;loop:true"></a-box>
-
-      <a-plane color="black" position="0 1 -3" width="3" height="0.25" class="clickable"
-        change-site="img: #ugc"
-        sound="on:mouseenter; src:#alerta">
-
-        <a-text value="Posgrados" color="white" align="center"></a-text>
-      </a-plane>
+            <a-text value="Ingenieria" color="gray" align="center"></a-text>
+          </a-plane>
 
 
+        </a-entity>
+
+        {/* -----------------------------------Posgrados---------------------------------- */}
+        <a-entity position="0 -0.3 0">
+          <a-box  scale="0.25 0.25 0.25" rotation="45 45 45" color="red" 
+            animation__position="property:rotation;to:360 0 0; dir:alternate;dur:3000;loop:true"></a-box>
+
+          <a-plane color="black" position="1.9 0 0" width="3" height="0.25" 
+            change-site="img: #ugc zone:.entrada-posgrados" class="entrada-posgrados"
+            sound="on:mouseenter; src:#alerta">
+
+            <a-text value="Posgrados" color="white" align="center"></a-text>
+          </a-plane>
+
+          <a-circle opacity="0" position="-0.9 -1 10"  scale="3 4 2"rotation ="0 2 0" radius="2" color="white"
+          class="entrada-posgrados" visible ="true"
+          change-site="img:#entradaposgrados; sound:#sound-entrada;zone: .salida-posgrados"
+          sound="on:mouseenter;src:#click">
+            <a-image src="#entrada"></a-image>
+
+          </a-circle>
+          <a-circle opacity="0" position="4 0 0" rotation ="0 -45 0" radius="2" color="white"
+          class="salida-posgrados" visible ="false"
+          change-site="img:#salidaposgrados; sound:#sound-salida;zone: .entrada-posgrados"
+          sound="on:mouseenter;src:#click">
+            <a-image src="#salida"></a-image>
+
+          </a-circle>
+
+        </a-entity>
 
 
 
-      {/* --------------------------------------------Campus--------------------------------- */}
-      <a-box position="-1.9 0.7 -3" scale="0.25 0.25 0.25" rotation="45 45 45" color="red"
-        animation__position="property:rotation;to:360 0 0; dir:alternate;dur:3000;loop:true"></a-box>
 
-      <a-plane color="black" position="0 0.7 -3" width="3" height="0.25" class="clickable"
-        change-site="img: #campus"
-        sound="on:mouseenter; src:#tuberia">
+        {/* --------------------------------------------Campus--------------------------------- */}
+        
+        <a-entity position="0 -0.6 0">
+          <a-box  scale="0.25 0.25 0.25" rotation="45 45 45" color="red" visible ="false"
+            animation__position="property:rotation;to:360 0 0; dir:alternate;dur:3000;loop:true"></a-box>
 
-        <a-text value="Campus" color="white" align="center"></a-text>
-      </a-plane>
+          <a-plane color="black" position="1.9 0 0" width="3" height="0.25" class="clickable"
+            change-site="img: #campus"
+            sound="on:mouseenter; src:#tuberia">
 
-      {/* ----------------------------------------Conciliacion--------------------------------- */}
-      <a-box position="-1.9 0.4 -3" scale="0.25 0.25 0.25" rotation="45 45 45" color="red"
-        animation__position="property:rotation;to:360 0 0; dir:alternate;dur:3000;loop:true"></a-box>
+            <a-text value="Campus" color="gray" align="center"></a-text>
+          </a-plane>
+        </a-entity>
+        {/* ----------------------------------------Conciliacion--------------------------------- */}
+        <a-entity position="0 -0.9 0">
+          <a-box  scale="0.25 0.25 0.25" rotation="45 45 45" color="red" visible= "false"
+            animation__position="property:rotation;to:360 0 0; dir:alternate;dur:3000;loop:true"></a-box>
 
-      <a-plane color="black" position="0 0.4 -3" width="3" height="0.25" class="clickable"
-        change-site="img: #conciliacion"
-        sound="on:mouseenter; src:#mario">
+          <a-plane color="black" position="1.9 0 0" width="3" height="0.25" class="clickable"
+            change-site="img: #conciliacion"
+            sound="on:mouseenter; src:#mario">
 
-        <a-text value="Conciliacion" color="white" align="center"></a-text>
-      </a-plane>
-
-      <a-sky id="my-sky" src="#campus"></a-sky>
-      </a-entuty>
+            <a-text value="Conciliacion" color="gray" align="center"></a-text>
+          </a-plane>
+        </a-entity>
+        </a-entity>
+        <a-sky id="my-sky" src="#ugc"></a-sky>
+    
     </a-scene>
 
 
--- proyecto-1/src/components/change-site.js
+++ proyecto-1/src/components/change-site.js
@@ -1,13 +1,53 @@
 import AFRAME from'aframe';
 AFRAME.registerComponent('change-site', {
     schema: {
-     img:{type:'string'}
+     img:{type:'string'},
+     zone:{type:'string'},
+     sound:{type:'string'}
     },
   
     init: function () {
         var data = this.data; // lo que llega de change-site:
         var el = this.el;  // elemento
         el.addEventListener("mouseenter",function(){
+            var parentEntity = el.parentNode; // del elemento que se ha echo clic queremos su padre
+            var grandParentEntity=parentEntity.parentNode //el padre del padre sobre el que se ha echo click
+            
+            var thisAPlane= parentEntity.querySelector("a-plane");//del que se ha echo clic coje el primer plano
+            thisAPlane.classList.remove("clickable"); // se quita la clase clckable
+
+            var allPlane = grandParentEntity.querySelectorAll("a-plane");// coje todos los planes del abuelo 
+            Object.keys(allPlane).forEach(function(key){ // coje cada uno de los planos que tiene el abuelo 
+                if(allPlane[key] != thisAPlane){ // se añade a todos los elementos que no se ha echo clic
+                    allPlane[key].classList.add("clickable")// la clase clickable
+                }
+            });
+            var allbox = grandParentEntity.querySelectorAll("a-box");
+            Object.keys(allbox).forEach(function(key){
+                allbox[key].setAttribute("visible","false")
+            })
+            var parentEntityABox= parentEntity.querySelector("a-box");
+            parentEntityABox.setAttribute("visible","true")
+
+            var allAText = grandParentEntity.querySelectorAll("a-text");
+            Object.keys(allAText).forEach(function(key){
+                allAText[key].setAttribute("color","gray")
+            })
+            var aText = thisAPlane.querySelector("a-text");
+            if(aText)aText.setAttribute("color","white")
+
+            var allACircle = grandParentEntity.querySelectorAll("a-circle"); // coje todos los circulos y los deja invisibles
+            Object.keys(allACircle).forEach(function(key){
+                allACircle[key].setAttribute("visible","false")
+                allACircle[key].classList.remove("clickable"); // y les quita la clase clickeable 
+            });
+
+            var allACirclInThisZone=parentEntity.querySelectorAll(data.zone);
+            Object.keys(allACirclInThisZone).forEach(function(key){
+                allACirclInThisZone[key].setAttribute("visible","true");
+                allACirclInThisZone[key].classList.add("clickable")
+            })
+            
             var mySky=document.querySelector("#my-sky");
             mySky.setAttribute("src",data.img)
         }); 
